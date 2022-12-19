9ceeb7240eead175f7f81aa9e8389dae5ba45b55
Aarav Dave <aarinaarav@gmail.com>
2022-12-19 12:53:07 -0500

    Update host.py

--- host.py
+++ host.py
@@ -1,64 +1,32 @@
-def receive_message(client_socket):
-    try:
-        message_header = client_socket.recv(HEADER_LENGTH)
+import pickle
+import socket
+import struct
+import cv2
 
-        if not len(message_header):
-            return False
+HOST = ''
+PORT = 8000
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(10)
+print('Waiting for members to join...')
 
-        message_length = int(message_header.decode('utf-8').strip())
-        return {'header': message_header, 'data': client_socket.recv(message_length)}
+conn, addr = s.accept()
+data = b''
+payload_size = struct.calcsize("L")
 
-    except:
-        return False
+while True:
+    while len(data) < payload_size:
+        data += conn.recv(4096)
 
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack('L', packed_msg_size)[0]
 
-import socket, select, datetime
+    while len(data) < msg_size:
+        data += conn.recv(4096)
 
-HEADER_LENGTH = 10  # Sets the chat size limit to 10 MB
-IP = '127.0.0.1'  # The standard IP address
-PORT = 1234  # Sets the port number; the more random, the better
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-server_socket.bind((IP, PORT))
-server_socket.listen()
-
-sockets_list = [server_socket]
-clients = {}
-
-while True:  # Runs the main loop forever until you exit
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
-
-    for notified_socket in read_sockets:
-        if notified_socket == server_socket:
-            client_socket, client_adress = server_socket.accept()
-
-            user = receive_message(client_socket)
-            if user is False:
-                continue
-
-            sockets_list.append(client_socket)
-            clients[client_socket] = user
-            print(f'\n{user["data"].decode("utf-8")} joined the chatroom\n')  # Prints that a user joined the chat
-
-        else:
-            message = receive_message(notified_socket)
-
-            if message is False:
-                print(
-                    f'\n{clients[notified_socket]["data"].decode("utf-8")} has left the chatroom\n')  # Prints that someone left the chat
-                sockets_list.remove(notified_socket)
-                del clients[notified_socket]
-                continue
-
-            user = clients[notified_socket]
-            print(f'{user["data"].decode("utf-8")} {datetime.datetime.now().strftime("%d/%m/%Y at %H:%M:%S")}\n{message["data"].decode("utf-8")}\n')  # Prints the recieved message
-
-            for client_socket in clients:
-                if client_socket != notified_socket:
-                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
-
-    for notified_socket in exception_sockets:
-        sockets_list.remove(notified_socket)
-        del clients[notified_socket]
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    frame = pickle.loads(frame_data)
+    cv2.imshow('frame', frame)
+    cv2.waitKey(1)
